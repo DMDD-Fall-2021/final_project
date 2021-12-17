b6cede6bb079d8affccff9457482954b7dcd1151
Shubham Jain <jain.shub@northeastern.edu>
2021-12-17 14:13:21 -0500

    Added 4 new views with location and habit to patient susceptible disease

--- execute_reports.sql
+++ execute_reports.sql
@@ -3,6 +3,10 @@ DROP VIEW "CITY_WISE_PATIENT";
 DROP VIEW "DISEASE_ACTIVE_CASES";
 DROP VIEW "TOP_3_HEALTIEST_CITIES";
 DROP VIEW "ZIP_WISE_ACTIVE_CASE";
+DROP VIEW "ZIP_WISE_PATIENT_AIR_QUALITY";
+DROP VIEW "ZIP_WISE_PATIENT_PARTICULATE_QUALITY";
+DROP VIEW "ZIP_WISE_PATIENT_WATER_QUALITY";
+DROP VIEW "HABIT_WISE_PATIENT_SUSCEPTIBLE_TO_DISEASES";
 --------------------------------------------------------
 --  DDL for View CITY_WISE_ACTIVE_CASE
 --------------------------------------------------------
@@ -55,3 +59,67 @@ AND z.country_code = c.country_code
 GROUP BY d.disease_name, Z.zip_id, z.city, z.county, c.country_name
 ORDER BY d.disease_name, z.zip_id
 ;
+
+
+--------------------------------------------------------
+--  DDL for View ZIP_WISE_PATIENT_AIR_QUALITY
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW ZIP_WISE_PATIENT_AIR_QUALITY AS SELECT p.first_name, p.last_name, z.county, z.city, z.zip_code,
+   CASE WHEN l.air_polution_index BETWEEN 0 AND 50 THEN 'Good'
+   WHEN l.air_polution_index BETWEEN 51 AND 100 THEN 'Moderate'
+   WHEN l.air_polution_index BETWEEN 101 AND 150 THEN 'Unhealthy for Sensitive Groups'
+   WHEN l.air_polution_index BETWEEN 151 AND 200 THEN 'Unhealthy'
+   WHEN l.air_polution_index BETWEEN 201 AND 300 THEN 'Very Unhealthy'
+   ELSE 'Hazardous' END as AIR_QUALITY
+   FROM user_details p join location_statistics l on p.zip_id=l.zip_id
+   join zipcode z on l.zip_id=z.zip_id;
+   
+--------------------------------------------------------
+--  DDL for View ZIP_WISE_PATIENT_PARTICULATE_QUALITY
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW ZIP_WISE_PATIENT_PARTICULATE_QUALITY AS SELECT p.first_name, p.last_name, z.county, z.city, z.zip_code,
+   CASE WHEN l.particulate_matter_reading BETWEEN 0 AND 50 THEN 'Good'
+   WHEN l.particulate_matter_reading BETWEEN 51 AND 100 THEN 'Moderate'
+   WHEN l.particulate_matter_reading BETWEEN 101 AND 150 THEN 'Unhealthy for Sensitive Groups'
+   WHEN l.particulate_matter_reading BETWEEN 151 AND 200 THEN 'Unhealthy'
+   WHEN l.particulate_matter_reading BETWEEN 201 AND 300 THEN 'Very Unhealthy'
+   ELSE 'Hazardous' END as PARTICULATE_QUALITY
+   FROM user_details p join location_statistics l on p.zip_id=l.zip_id
+   join zipcode z on l.zip_id=z.zip_id;
+    
+--------------------------------------------------------
+--  DDL for View ZIP_WISE_PATIENT_WATER_QUALITY
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW ZIP_WISE_PATIENT_WATER_QUALITY AS SELECT p.first_name, p.last_name, z.county, z.city, z.zip_code,
+   CASE WHEN l.water_quality_parameter BETWEEN 0 AND 50 THEN 'Excellent Water'
+   WHEN l.water_quality_parameter BETWEEN 51 AND 100 THEN 'Good Water'
+   WHEN l.water_quality_parameter BETWEEN 101 AND 200 THEN 'Poor Water'
+   WHEN l.water_quality_parameter BETWEEN 201 AND 300 THEN 'Very Poor Water'
+   ELSE 'Unsuitable to drink' END as WATER_QUALITY
+   FROM user_details p join location_statistics l on p.zip_id=l.zip_id
+   join zipcode z on l.zip_id=z.zip_id; 
+
+
+--------------------------------------------------------
+--  DDL for View HABIT_WISE_PATIENT_SUSCEPTIBLE_TO_DISEASES
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW HABIT_WISE_PATIENT_SUSCEPTIBLE_TO_DISEASES AS
+    select p.first_name, p.last_name, d.disease_name as SUSCEPTIBLE_TO_DISEASE
+    from user_details p, diseases d
+    where p.user_id in
+        (select hp.patient_id
+        from habits_patient hp
+        join habit_disease hd
+        on hp.habit_id=hd.habit_id
+        where hp.frequency_per_week>=hd.impact_threshold_per_week)
+    and d.disease_id in
+        (select hd.disease_id
+        from habits_patient hp
+        join habit_disease hd
+        on hp.habit_id=hd.habit_id
+        where hp.frequency_per_week>=hd.impact_threshold_per_week)
+    order by p.first_name;
